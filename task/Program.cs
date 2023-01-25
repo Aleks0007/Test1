db571e40d0ece854d080473ae5a2f43d5acd33fc
AlekseiShvaiko <aarkaklim@gmail.com>
2023-01-25 17:03:28 +0400

    Добавлен метод FilteredArray

--- task/Program.cs
+++ task/Program.cs
@@ -15,4 +15,25 @@
         Console.WriteLine("Результирующий массив:");
         Console.WriteLine(string.Join(" ", array));
     }
+
+    public static string[] FilteredArray(string[] initialArray)
+    {
+        int count = 0;
+        for (int i = 0; i < initialArray.Length; i++)
+        {
+            if (initialArray[i].Length <= 3) count++;
+        }
+
+        int j = 0;
+        string[] filteredArray = new string[count];
+        for (int i = 0; i < initialArray.Length; i++)
+        {
+            if (initialArray[i].Length <= 3)
+            {
+                filteredArray[j] = initialArray[i];
+                j++;
+            }
+        }
+        return filteredArray;
+    }
 }
